47545f695b0a99dd4e268e9f7fd2cb358ae1c3c4
matthieu solente <yuya1@hotmail.fr>
2024-06-21 21:22:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
     bgColor = $('#bgColor').val();
     tbPadding = $('#tbPadding').val();
     lrPadding = $('#lrPadding').val();
-    width =  Math.ceil($(cssBtnClass).outerWidth());
+    width =  Math.ceil($(cssBtnClass).outerWidth()-lrPadding);
     height =  Math.ceil($(cssBtnClass).outerHeight());
     vmlheight = Math.ceil($(cssBtnClass).outerHeight()+2);
     bgcss = 'background-color:'+$('#bgColor').val();
@@ -36,22 +36,10 @@
     } 
     generatedButtonPreview ='<!--[if mso ]><table role="presentation" cellspacing="0" cellpadding="0" border="0" style="font-size:0;"><tr><td style="width:'+width+'px;height:'+height+'px;border-radius:'+bRadius+'px;background:'+bgColor+';text-align:center"><![endif]--><a '+btnClass+' href="'+urlLink+'" style="border:1px solid '+bgColor+';font-family:'+fontFamily+';font-size:'+fontSize+'px; mso-line-height-rule:exactly;line-height:'+fontSize+'px;'+fontBold+' '+textTransform+' text-decoration:none;text-underline-color:'+bgColor+';color:'+fontColor+';display:inline-block;background-color:'+bgColor+';border-radius:'+bRadius+'px;padding:'+tbPadding+'px '+lrPadding+'px;mso-border-alt:0">'+btnText+'</a><!--[if mso ]><v:shape stroked="f" filled="t" coordorigin="0 0" coordsize="'+bRadius+' '+vmlheight+'" style="width:'+bRadius+'px;height:'+vmlheight+'px;position:absolute;top:0;left:-'+bRadius+'px;margin-right:-1px" fillcolor="'+bgColor+'" path="m'+bRadius+',0 '+bRadius+','+vmlheight+' qx0,'+vmlbottomradius+' 0,'+bRadius+' qy'+bRadius+',0xe"></v:shape><![endif]--><!--[if (gte mso 9)|(IE)]><v:shape  stroked="f" filled="t" coordorigin="0 0" coordsize="'+bRadius+' '+vmlheight+'" style="width:'+bRadius+'px;height:'+vmlheight+'px;position:absolute;top:0;left:'+width+'px;margin-left:-1px" fillcolor="'+bgColor+'" path="qx'+bRadius+','+bRadius+' '+bRadius+','+vmlbottomradius+' qy0,'+vmlheight+' xe"></v:shape><![endif]--><!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->';
     generatedButton = '&lt;!--[if mso ]&gt;&lt;table role="presentation" cellspacing="0" cellpadding="0" border="0" style="font-size:0;"&gt;&lt;tr&gt;&lt;td style="width:'+width+'px;height:'+height+'px;border-radius:'+bRadius+'px;background:'+bgColor+';text-align:center"&gt;&lt;![endif]--&gt;&lt;a '+btnClass+' href="'+urlLink+'" style="border:1px solid '+bgColor+';font-family:'+fontFamily+';font-size:'+fontSize+'px; mso-line-height-rule:exactly;line-height:'+fontSize+'px;'+fontBold+' '+textTransform+' text-decoration:none;text-underline-color:'+bgColor+';color:'+fontColor+';display:inline-block;background-color:'+bgColor+';border-radius:'+bRadius+'px;padding:'+tbPadding+'px '+lrPadding+'px;mso-border-alt:0"&gt;'+btnText+'&lt;/a&gt;&lt;!--[if mso ]&gt;&lt;v:shape stroked="f" filled="t" coordorigin="0 0" coordsize="'+bRadius+' '+vmlheight+'" style="width:'+bRadius+'px;height:'+vmlheight+'px;position:absolute;top:0;left:-'+bRadius+'px;margin-right:-1px" fillcolor="'+bgColor+'" path="m'+bRadius+',0 '+bRadius+','+vmlheight+' qx0,'+vmlbottomradius+' 0,'+bRadius+' qy'+bRadius+',0xe"&gt;&lt;/v:shape&gt;&lt;![endif]--&gt;&lt;!--[if (gte mso 9)|(IE)]&gt;&lt;v:shape  stroked="f" filled="t" coordorigin="0 0" coordsize="'+bRadius+' '+vmlheight+'" style="width:'+bRadius+'px;height:'+vmlheight+'px;position:absolute;top:0;left:'+width+'px;margin-left:-1px" fillcolor="'+bgColor+'" path="qx'+bRadius+','+bRadius+' '+bRadius+','+vmlbottomradius+' qy0,'+vmlheight+' xe"&gt;&lt;/v:shape&gt;&lt;![endif]--&gt;&lt;!--[if (gte mso 9)|(IE)]&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;![endif]--&gt;';
-    if($('#fontColor').val()==='#ffffff'||$('#fontColor').val()==='#fff'){
-      generateCss = '&lt;!--[if gte mso 16]&gt;\n&lt;style&gt;\n '+cssBtnClass+'{\nbackground: transparent !important;\nmso-style-textfill-type:gradient;\n mso-style-textfill-fill-gradientfill-stoplist:&quot;0 \#FFFFFF 0 100000\,100000 \#FFFFFF 0 100000&quot;;\ncolor:#000000 !important;\n background: transparent !important;\n}\n&lt;/style&gt;\n&lt;![endif]--&gt;';
-    }else if($('#fontColor').val()==='#000000'){
-      generateCss = '&lt;!--[if gte mso 16]&gt;\n&lt;style&gt;\n'+cssBtnClass+'{\nbackground: transparent !important;\nmso-style-textfill-type:gradient;\n mso-style-textfill-fill-gradientfill-stoplist:&quot;0 \#000000 1 100000\,99000 \#000000 1 100000&quot;;\ncolor:#ffffff !important;\n background: transparent !important;\n}\n&lt;/style&gt;\n&lt;![endif]--&gt;';
-    }else{
-      generateCss = '&lt;!--[if gte mso 16]&gt;\n&lt;style&gt;\n'+cssBtnClass+'{\nbackground: transparent !important;\n}\n&lt;/style&gt;\n&lt;![endif]--&gt;';
-    }
+   
     $('#preview').html(generatedButtonPreview);
     $('#code').html(generatedButton);
-    if($('#fontColor').val()){
-      $('#css-code').show();
-      $('#css').html(generateCss);
-    }else{
-      $('#css-code').hide();
-      $('#css').html('');
-    }
+   
 }updateValues();
 let filtersChange = $('.wrapper').find('select,input');
 filtersChange.on('input change', function(e) {
